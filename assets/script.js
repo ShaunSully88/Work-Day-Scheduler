fb5425fde103644a0569008dbfc9d30cda12d216
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-25 14:13:55 -0400

    textarea is now grey, which is progress. Still not linking with other CSS classes though

--- assets/script.js
+++ assets/script.js
@@ -8,22 +8,39 @@ var todayTime =moment().format('LT');
 $("#time").html(todayTime);
 console.log(todayTime);
 
-var textArea = document.getElementById('textArea')
+
 
 //Timeblock time
-var dateTime = document.getElementById('dateTime');
+
+for (var i = 9; i <= 17; i++); {
+    
+if(i > todayTime){
+    $("").addClass('future');
+}
+else if(i == todayTime){
+    $("textarea").addClass('present');
+}
+else{ 
+    $("textarea").addClass('past');   
+}};
 
 
 
-if(dateTime < todayTime) {
-    textArea.classList.add("future")
-};
-
-if(dateTime === todayTime) {
-    textArea.classList.add("present")
-};
-
-if(dateTime > todayTime) {
-    textArea.classList.add("past")
-};
-
+//var saveTasks = function() {
+  //  localStorage.setItem("tasks", JSON.stringify(tasks));
+//  };
+  
+ // $(".list-group").on("click", "p", function() {
+  //  var text = $(this)
+  //  .text()
+ //   .trim();
+  //  console.log(text);
+  
+  //  var textInput = $("<textarea>")
+   // .addClass("form-control")
+  //  .val(text);
+   // $(this).replaceWith(textInput);
+  
+   // textInput.trigger("focus");
+    
+ // });
