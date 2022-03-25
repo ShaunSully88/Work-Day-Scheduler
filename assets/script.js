5ba530e3922cc632f01eb654c09dcfcd8ceb7a0f
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-25 11:49:08 -0400

    Id's set up for each timeblock, function being made to compare timeblock to timeclock for adding CSS attributes

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,29 @@
 //Today's Date displayed on main page
-
 var todayDate =moment().format('dddd, MMM Do YYYY');
 $("#today").html(todayDate);
 console.log(todayDate);
+
+//Today's time
+var todayTime =moment().format('LT');
+$("#time").html(todayTime);
+console.log(todayTime);
+
+var textArea = document.getElementById('textArea')
+
+//Timeblock time
+var dateTime = document.getElementById('dateTime');
+
+
+
+if(dateTime < todayTime) {
+    textArea.classList.add("future")
+};
+
+if(dateTime === todayTime) {
+    textArea.classList.add("present")
+};
+
+if(dateTime > todayTime) {
+    textArea.classList.add("past")
+};
+
